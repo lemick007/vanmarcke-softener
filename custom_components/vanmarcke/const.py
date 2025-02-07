23bf7dff1496e4ba922fecf120036d54f4124309
lemick007 <97547214+lemick007@users.noreply.github.com>
2025-02-07 00:18:49 +0000

    0.0.2

--- custom_components/vanmarcke/const.py
+++ custom_components/vanmarcke/const.py
@@ -1,19 +1,22 @@
-# const.py - Configuration centrale
+# const.py
 DOMAIN = "vanmarcke_water"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+
 SENSOR_MAP = {
-    "current_flow": {
-        "name": "Débit actuel", 
-        "unit": "L/min",
+    "salt_level": {
+        "name": "Niveau de sel", 
+        "unit": "%",
+        "icon": "mdi:shaker-outline"
+    },
+    "water_hardness": {
+        "name": "Dureté de l'eau",
+        "unit": "°fH",
         "icon": "mdi:water"
     },
-    "total_consumption": {
-        "name": "Consommation totale",
-        "unit": "L",
-        "icon": "mdi:chart-bar"
-    },
-    "salt_level": {
-        "name": "Niveau de sel",
-        "unit": "%",
-        "icon": "mdi:cup-water"
+    "water_flow": {
+        "name": "Débit d'eau",
+        "unit": "L/min",
+        "icon": "mdi:hydraulic-oil-flow"
     }
 }
